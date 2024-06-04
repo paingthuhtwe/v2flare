c147cec381474caf8f4f24ada02da0a29e2840ff
Paing Thu Htwe <95089258+paingthuhtwe@users.noreply.github.com>
2024-06-04 17:42:05 +0630

    Update _worker.js
    change user id

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'b0286bc2-e17c-4c17-b0e6-d39c93f74937';
+let userID = 'b0286bc2-e17c-4c17-b0e6-d39c93f82727';
 
 const พร็อกซีไอพีs = ['cdn.xn--b6gac.eu.org', 'cdn-all.xn--b6gac.eu.org', 'workers.cloudflare.cyou'];
 
